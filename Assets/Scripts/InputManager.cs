34e1c3bd8d77ee001d9dc77ea0befd22343f6b2a
Luca Rickler <luca.rickler@gmail.com>
2019-01-25 22:29:09 +0100

    Small fix on input manager

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,23 +1,23 @@
 using UnityEngine;
 
-public static class InputManager : MonoBehaviour {
+public static class InputManager {
   public static bool Up() {
-
+    return false;
   }
   
   public static bool Down() {
-
+    return false;
   }
 
   public static bool Left() {
-
+    return false;
   }
 
   public static bool Right() {
-
+    return false;
   }
 
   public static bool Grab() {
-    
+    return false;
   }
 }
